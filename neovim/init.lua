6db636cf0a4f241365729dcd8e9489c82a3bd2c5
Adam Cooper <adamc@takescoop.com>
2023-12-22 17:13:54 -0500

    [WIP] dap

--- neovim/init.lua
+++ neovim/init.lua
@@ -126,6 +126,27 @@ require("lazy").setup({
       opts = {},
       config = function(_, opts) require'lsp_signature'.setup(opts) end
     },
+    {
+        "rcarriga/nvim-dap-ui",
+        event = "VeryLazy",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            require("dapui").setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+    "mfussenegger/nvim-dap",
+    { "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } },
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
@@ -259,6 +280,44 @@ vim.api.nvim_create_autocmd('LspAttach', {
       end, opts('LSP: format'))
     end,
 })
+
+
+-- nvim-dap
+require('dap-vscode-js').setup({
+    debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter',
+    debugger_cmd = { 'js-debug-adapter' },
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+})
+
+local dap = require("dap")
+dap.adapters["pwa-node"] = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 8123,
+    executable = {
+        command = "js-debug-adapter",
+    }
+}
+
+for _, language in ipairs { "typescript", "javascript" } do
+    dap.configurations[language] = {
+        {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            sourceMaps = true,
+            protocol = 'inspector',
+            -- runtimeExecutable = "node",
+        },
+    }
+end
+
+vim.keymap.set('n', '<leader>db', '<cmd>DapToggleBreakpoint<CR>')
+vim.keymap.set('n', '<leader>dr', '<cmd>DapContinue<CR>')
+
 
 -- Set up nvim-cmp.
 local cmp = require'cmp'
